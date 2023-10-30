d5fa1c8d6b4bccb9768fa34c86a0386e67a086bc
José victor andrade oliveira <victorandrade150@gmail.com>
2023-10-29 22:31:10 -0300

    initial commit

---
+++ srcts/routes/index.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { Customer } from '../models/cliente';
+import { format } from 'date-fns';
+
+
+const customers : Customer[] = [];
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.json(customers);
+})
+
+router.post('/', (req, res) => {
+    const newCustomer : Customer = {
+        id: parseInt(req.body.id),
+        name: req.body.name,
+        birthDate: new Date(Date.parse(req.body.birthDate))
+    };
+    customers.push(newCustomer);
+    res.status(201).json(newCustomer);
+})
+
+export default router;
